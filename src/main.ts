5c4653cdd166cafd2ded8a221cd8064781d80b21
Prashant Deshpande <vacationlocation.devteam@gmail.com>
2026-02-25 09:43:22 +0530

    changed cross origin code

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || true, // true = allow all origins (dev); set FRONTEND_URL in prod
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
